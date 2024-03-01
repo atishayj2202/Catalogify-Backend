f192a07ae288a2f276f9f5f04edb71460e09d2d0
ATISHAY JAIN <atishayj.dev@gmail.com>
2024-03-02 02:48:23 +0530

    Updated Enums

--- src/utils/enums.py
+++ src/utils/enums.py
@@ -8,20 +8,3 @@ class AuthType(Enum):
     apple = "apple"
     email = "email"
     phone = "phone"
-
-
-class UserType(Enum):
-    employee = "employee"
-    employer = "employer"
-
-
-class TaskStatus(Enum):
-    pending = "pending"
-    completed = "completed"
-    cancelled = "cancelled"
-
-
-class EmployeeStatus(Enum):
-    active = "active"
-    removed = "removed"
-    left = "left"
