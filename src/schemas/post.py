b4a128211d2587023702eaf744b558864e22fd0b
ATISHAY JAIN <atishayj.dev@gmail.com>
2024-03-06 14:47:29 +0530

    Added Post Algorithms

--- src/schemas/post.py
+++ src/schemas/post.py
@@ -43,3 +43,13 @@ class PostLongResponse(BaseModel):
     return_days: int = 0
     seller_location: str | None = None
     in_box: str | None = None
+
+
+class PostEditRequest(BaseModel):
+    title: str | None = None
+    images: list[str] | None = None
+    description: str | None = None
+    cost: float | None = None
+    brand: str | None = None
+    seller_location: str | None = None
+    in_box: str | None = None
---
+++ src/services/post.py
@@ -0,0 +1,44 @@
+from src.client.cockroach import CockroachDBClient
+from src.db.table.post import Post
+from src.schemas.post import PostEditRequest, PostLongResponse
+
+
+class PostService:
+    @classmethod
+    def fetch_post(cls, post: Post) -> PostLongResponse:
+        return PostLongResponse(
+            id=post.id,
+            created_at=post.created_at,
+            user_id=post.user_id,
+            title=post.title,
+            category=post.category,
+            images=post.images,
+            description=post.description,
+            cost=post.cost,
+            brand=post.brand,
+            warranty_yrs=post.warranty_yrs,
+            warranty_months=post.warranty_months,
+            return_days=post.return_days,
+            seller_location=post.seller_location,
+            in_box=post.in_box,
+        )
+
+    @classmethod
+    def edit_post(
+        cls, post: Post, request: PostEditRequest, cockroach_client: CockroachDBClient
+    ):
+        if request.title is not None:
+            post.title = request.title
+        if request.images is not None:
+            post.images = request.images
+        if request.description is not None:
+            post.description = request.description
+        if request.cost is not None:
+            post.cost = request.cost
+        if request.brand is not None:
+            post.brand = request.brand
+        if request.seller_location is not None:
+            post.seller_location = request.seller_location
+        if request.in_box is not None:
+            post.in_box = request.in_box
+        cockroach_client.query(Post.update_by_id, id=post.id, new_data=post)
